7d2b9ea05579eca9c081defe34fef6b0495d632d
shivkumar0757 <shivkumar0757@gmail.com>
2022-01-25 08:15:22 +0530

    initial commit

---
+++ w3_utility/staking_status_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StakingStatusAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'staking_status_app'
---
+++ w3_utility/staking_status_app/ent_test.py
@@ -0,0 +1,10 @@
+import requests
+import smtplib
+import requests as req
+import json
+import datetime
+from flask import Flask, render_template
+import requests as req
+import json
+from flask import jsonify
+import datetime
