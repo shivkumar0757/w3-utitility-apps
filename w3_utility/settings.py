3f5a5c8de4865c959d337580a22eb505a6bb0257
shivkumar0757 <shivkumar0757@gmail.com>
2022-02-05 16:58:49 +0530

    base 1

--- w3_utility/settings.py
+++ w3_utility/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR1= os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -85,6 +85,7 @@ DATABASES = {
     }
 }
 print('---------------------BASE_DIR:   ',BASE_DIR)
+print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$,  BASE_DIR1', BASE_DIR1)
 
 
 # Password validation
