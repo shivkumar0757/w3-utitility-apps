e4a269d11b26b5df7bac6b7f56d87d176c04f9f6
shivkumar0757 <shivkumar0757@gmail.com>
2022-02-07 22:10:15 +0530

    demo

---
+++ temp.py
@@ -0,0 +1,11 @@
+from clock import send_email,emdata,logdata
+
+print("-------------- Sending email-----------")
+
+send_email()
+
+print("email sent---------")
+
+print('logData: ',send_email)
+
+print('emdata: ', emdata)
