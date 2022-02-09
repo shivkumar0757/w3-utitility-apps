ed3b8265839b1a52fdedb2831a4a9713e63e4af6
shivkumar0757 <shivkumar0757@gmail.com>
2022-02-09 08:47:05 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -4,10 +4,11 @@ from staking_status_app.email import Emails
 
 print("-------------- Sending email-----------")
 
-#send_email()
+
 data = 'Testing email api'
-em= Emails()
-em.sendMail('shivkumar0757@gmail.com',data )
+# em= Emails()
+# em.sendMail('shivkumar0757@gmail.com',data )
+send_email()
 
 print("email sent---------")
 
