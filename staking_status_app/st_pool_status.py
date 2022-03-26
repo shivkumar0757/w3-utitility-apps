79b59782562da1003f773a138039645802005e85
shivkumar0757 <shivkumar0757@gmail.com>
2022-03-26 07:33:43 +0530

    dtaking page

--- staking_status_app/st_pool_status.py
+++ staking_status_app/st_pool_status.py
@@ -5,23 +5,29 @@ import time
 
 base_url= 'https://www.binance.com/bapi/earn/v1/friendly/pos/union'
 params = {'status':'ALL','asset':'axs'}
+log_data=[]
+
 
 def get_staking_data():
+    proj=get_status()
+    email_data=[]
+    for i in proj:
+        if i['available']:
+            email_data.append(i)
+        log_data.append(i)
+    return email_data, log_data
+
+
+def get_status():
     response = req.get(base_url,params=params)
     rr=json.loads(response.content)
-    email_data=[]
-    log_data=[]
+    data=[]
     if len(rr['data'])>0:
         for asset in rr['data']:
             if len(asset['projects']) > 0:
                 for proj in asset['projects']:
-
-                    if not proj['sellOut']:
-                        email_data.append({'asset': proj['asset']+' '+ proj['duration'],
+                    data.append({'asset': proj['asset']+' '+ proj['duration'],
                                           'available': not proj['sellOut'] ,
-                                          'time':str(datetime.datetime.now())})
-                    log_data.append({'asset': proj['asset']+' '+ proj['duration'],
-                                          'available': not proj['sellOut'] ,
-                                          'time':str(datetime.datetime.now())})
-    return email_data,log_data
+                                          'time':str(datetime.datetime.now().strftime("%H:%M:%S  , %d-%b-%Y "))})
+    return data
 
--- staking_status_app/views.py
+++ staking_status_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from staking_status_app.st_pool_status import get_status
+
 # Create your views here.
 def index(request):
-    return render(request,'stake/index.html')
+    st_data= get_status()
+    return render(request,'stake/index.html',{'data':st_data})
